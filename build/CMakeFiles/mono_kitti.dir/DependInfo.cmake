96fc0c4241502fde3160b4b004b3111e5282a9fe
freviriego <fidel_97brea@hotmail.com>
2020-04-18 20:10:28 +0200

    Initial code (ORB-SLAM2), camera calibration and compilation errors fixed

---
+++ build/CMakeFiles/mono_kitti.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/freviriego/ORB_SLAM2/Examples/Monocular/mono_kitti.cc" "/home/freviriego/ORB_SLAM2/build/CMakeFiles/mono_kitti.dir/Examples/Monocular/mono_kitti.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/home/freviriego/Pangolin/include"
+  "/home/freviriego/Pangolin/build/src/include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/freviriego/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
