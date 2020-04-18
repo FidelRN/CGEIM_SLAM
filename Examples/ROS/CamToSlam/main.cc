d6cc6ccc39a2691513640d92bd560e8c31d3b98a
freviriego <fidel_97brea@hotmail.com>
2020-04-18 21:07:15 +0200

    Compile

---
+++ Examples/ROS/CamToSlam/main.cc
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<ros/ros.h>
+#include<rosbag/bag.h>
+#include<rosbag/view.h>
+#include<sensor_msgs/Image.h>
+#include<std_msgs/Time.h>
+#include<std_msgs/Header.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#include "Thirdparty/DLib/FileFunctions.h"
+
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "CamToSlam");
+
+
+    ros::start();
+
+    ros::Time t = ros::Time::now();
+
+    cv::VideoCapture vcap;
+    cv::Mat image;
+
+    const std::string videoStreamAddress = "http://localhost:5555/video";
+       //open the video stream and make sure it's opened
+    if(!vcap.open(videoStreamAddress)) {
+        std::cerr << "Error opening video stream or file" << std::endl;
+        return -1;
+    }
+
+    ros::NodeHandle n;
+    ros::Publisher pub = n.advertise<sensor_msgs::Image>("/camera/image_raw", 1000);
+
+    for(;;)
+    {
+        if(!ros::ok())
+            break;
+
+        vcap.read(image);
+        resize(image, image, Size(640, 360), 0, 0, INTER_CUBIC);
+        
+        cv_bridge::CvImage cvImage;
+        cvImage.image = image;
+        cvImage.encoding = sensor_msgs::image_encodings::RGB8;
+        cvImage.header.stamp = t;
+
+        // Publish to topic the image
+        pub.publish(cvImage.toImageMsg());
+        cv::waitKey(30);
+    }
+    ros::shutdown();
+
+    return 0;
+}
